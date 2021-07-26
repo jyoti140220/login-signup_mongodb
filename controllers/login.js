5795dd4e3b298f1d803b257c6aacb33279895eb0
jyoti140220 <jyotibhandari20@navgurukul.org>
2021-07-26 10:09:42 +0530

    changes

--- controllers/login.js
+++ controllers/login.js
@@ -18,30 +18,6 @@ const login = async (req, res) => {
     }else{
         return res.status(404).json({message: "Invalid Email-Id",status: 404})
     }
-
-
-//     if (data1 == null) {
-//         return res.status(404).json({
-//             message: "Invalid Email-Id",
-//             status: 404
-//         })
-//     } else {
-//         const comaparePassword = await bcrypt.compare(req.body.password, data1.password)
-//         if (comaparePassword) {
-//             const token=await jwttoken({ email: req.body.email },process.env.SECRET_KEY)
-//             console.log(token)
-//             return res.status(200).json({
-//                 message: "You Have Logged Successfully!!!",
-//                 status: 200,
-//                 token: token
-//             })
-//         } else {
-//             return res.status(404).json({
-//                 message: "Invalid Password",
-//                 status: 404
-//             })
-//         }
-//     }
 }
 
 module.exports = login
